4c5d23ecb4031350706b5ff2d982715936bfe574
Lucky <lucky-lam@dealercell.com.au>
2018-06-28 17:52:35 +0700

    init

---
+++ TodoApi/Models/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public interface ITodoRepository
+    {
+        void AddItem( TodoItem item );
+        IEnumerable<TodoItem> GetAllItems();
+        TodoItem FindItem( string key );
+        TodoItem RemoveItem( string key );
+        void UpdateItem( TodoItem item );
+
+    }
+}
