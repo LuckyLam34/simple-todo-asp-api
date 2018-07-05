cebddb0c97712948b32213e72d71c5763d6a2b23
Lucky <lucky-lam@dealercell.com.au>
2018-07-05 18:00:13 +0700

    add create api

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -8,11 +8,11 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Todo")]
+    [Produces( "application/json" )]
+    [Route( "api/Todo" )]
     public class TodoController : Controller
     {
-        public TodoController( ITodoRepository todoItems)
+        public TodoController( ITodoRepository todoItems )
         {
             TodoItems = todoItems;
         }
@@ -23,15 +23,27 @@ namespace TodoApi.Controllers
             return TodoItems.GetAllItems();
         }
 
-        [HttpGet("{id}", Name ="GetTodo")]
-        public IActionResult GetById(string id)
+        [HttpGet( "{id}", Name = "GetTodo" )]
+        public IActionResult GetById( string id )
         {
             var item = TodoItems.FindItem( id );
-            if (item == null)
+            if( item == null )
             {
                 return NotFound();
             }
             return new ObjectResult( item );
+        }
+
+        [HttpPost]
+        public IActionResult Create( [FromBody] TodoItem item )
+        {
+            if( item == null )
+            {
+                return BadRequest();
+            }
+            TodoItems.AddItem( item );
+
+            return CreatedAtRoute( "GetTodo", new { id = item.Key }, item );
         }
     }
 }
